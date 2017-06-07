e5b253e35fa696d4ca64b59fde06b4b0cc170b3e
Анастасия <bisivanovo@yandex.ru>
2017-06-07 20:28:18 +0300

    JQuery

--- application/views/admin/classesview.php
+++ application/views/admin/classesview.php
@@ -1,3 +1,4 @@
+<script src="<?php echo base_url();?>bootstrap/js/jquery-2.1.1.min.js" type="text/javascript"></script>
 <div class="container">
 	<h3 class="sidebar-header"><i class="fa fa-users"></i> Список классов</h3>
 	<div class="panel panel-default panel-table">
