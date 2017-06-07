07655b46a878a725e11137df2a826329e54866c4
Анастасия <bisivanovo@yandex.ru>
2017-06-07 20:37:11 +0300

    Jquery

--- application/views/teacher/pupilsview.php
+++ application/views/teacher/pupilsview.php
@@ -7,6 +7,7 @@
 		echo $s;
 	}
 ?>
+<script src="<?php echo base_url();?>bootstrap/js/jquery-2.1.1.min.js" type="text/javascript"></script>
 <div class="container">
 	<h3 class="sidebar-header"><i class="fa fa-user"></i> Список учащихся</h3>
 	<div class="panel panel-default panel-table">
