4d7975d6b5a35177ed315f0541f43c5c64558262
Анастасия <bisivanovo@yandex.ru>
2017-06-07 20:42:07 +0300

    Select2

--- application/views/teacher/timetableview.php
+++ application/views/teacher/timetableview.php
@@ -2,6 +2,7 @@
 	$days = array("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота");
 	 ?>
 <script src="<?php echo base_url();?>bootstrap/js/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>js/select2.min.js" type="text/javascript"></script>
 <div class="container">
 	<div class="row" style="margin-bottom: 20px;">
 		<div class="col-md-6">
